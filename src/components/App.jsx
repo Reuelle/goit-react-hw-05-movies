82a9ec2b51d0f9729ce6239f555da48576e28fe7
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-03 13:01:56 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,9 @@ export const App = () => {
       }}
     >
       React homework template-movies
+      APIkey=d89da14cf20732d29d9230587b122e3f
+      API read access oken= eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODlkYTE0Y2YyMDczMmQyOWQ5MjMwNTg3YjEyMmUzZiIsIm5iZiI6MTcxOTk3NTA0Ni4zMTI2LCJzdWIiOiI2Njg0YmI1ZjQxMWJlYzMxMTE3NWU3MjYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TD2KzKsEk9ZAiAd8kPmYEEHHcbAtcW2LtT0KjtEzMDA
+      
     </div>
   );
 };
