799addb80b1bd172da308d4c821dac7cdc387a5c
Ruel Paya <ruel.paya@gmail.com>
2024-07-05 05:00:44 +0800

    styled
    header.jsx

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const HeaderContainer = styled.header`
+background-color:#333;
+color: #fff;
+padding: 15px;
+`;
